97354e187adf8dbcb37bb538758be7f6bd23b649
Pokenob15 <91990253+Pokenob15@users.noreply.github.com>
2021-10-11 12:00:12 -0400

    Add files via upload
    The start of the app. Need to delete the line with "menu." to run. Does create a window that is closeable however. I started it but i needed to go to class.

---
+++ Driver.java
@@ -0,0 +1,33 @@
+/** 
+ * Author: Dakota Henderson
+ * Date: 10/11/21
+ * 
+ * Is the first instance of App Exhibition. The main job is to get a 
+ * window open with a closeable menu.
+ */
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		JMenuBar menubar = new JMenuBar();
+		JMenu menu = new JMenu();
+		
+		menu.
+		
+		
+		frame.add(panel);
+		frame.add(menubar);
+		panel.setVisible(true);
+		frame.setVisible(true);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+}
